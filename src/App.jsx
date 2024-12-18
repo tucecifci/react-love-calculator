ee0a3d75305eb825c52cc7e2b01f64983737ee57
tucecifci <tucecifcii@gmail.com>
2024-12-18 16:39:28 +0300

    calculate completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import Calculate from "./Calculate";
 function App() {
   const [name1, setName1] = useState("");
   const [name2, setName2] = useState("");
+  const [result, setResult] = useState(null);
 
   return (
     <div className="container">
       <Header />
       <Names name1={name1} setName1={setName1} name2={name2} setName2={setName2} />
-      <Calculate />
-    </div>
+      <Calculate result={result} setResult = {setResult} name1={name1} name2={name2} />
+    </div> 
   );
 }
 
--- src/Calculate.jsx
+++ src/Calculate.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Calculate() {
+function Calculate({ name1, name2, result, setResult }) {
+    const [isCalculated, setIsCalculated] = useState(false);
+
+  const handleCalculate = () => {
+    if (name1 != "" && name2 != "") {
+      const result = Math.floor(Math.random() * 101);
+      setResult(result);
+      setIsCalculated(true);
+    } else {
+      alert("Lütfen iki ismi de doldurun");
+    }
+  };
+
   return (
-   <>
-     <button>Calculate</button>
+    <>
+      <button onClick={handleCalculate}>Calculate</button>
 
-   </>
-  
-  )
+      {isCalculated && (
+        <p className="result-message"><span className="names">{name1}</span> and <span className="names">{name2}</span>'s chance of love: <p className="result-percentage">{result} %</p>  </p>
+      )}
+    </>
+  );
 }
 
-export default Calculate
+export default Calculate;
