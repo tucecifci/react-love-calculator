d0cf7dc9ef43aa412b548a8e0e8df79d9a3da1ec
tucecifci <tucecifcii@gmail.com>
2024-12-18 16:53:53 +0300

    bug fixed

--- src/Calculate.jsx
+++ src/Calculate.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 function Calculate({ name1, name2, result, setResult }) {
-    const [isCalculated, setIsCalculated] = useState(false);
+  // const [isCalculated, setIsCalculated] = useState(false);
 
   const handleCalculate = () => {
     if (name1 != "" && name2 != "") {
       const result = Math.floor(Math.random() * 101);
       setResult(result);
-      setIsCalculated(true);
+      // setIsCalculated(true);
     } else {
       alert("Lütfen iki ismi de doldurun");
     }
@@ -17,8 +17,12 @@ function Calculate({ name1, name2, result, setResult }) {
     <>
       <button onClick={handleCalculate}>Calculate</button>
 
-      {isCalculated && (
-        <p className="result-message"><span className="names">{name1}</span> and <span className="names">{name2}</span>'s chance of love: <p className="result-percentage">{result} %</p>  </p>
+      {result !== null && (
+        <p className="result-message">
+          <span className="names">{name1}</span> and{" "}
+          <span className="names">{name2}</span>'s chance of love:{" "}
+          <p className="result-percentage">{result} %</p>{" "}
+        </p>
       )}
     </>
   );
--- src/Names.jsx
+++ src/Names.jsx
@@ -2,12 +2,12 @@ import React from "react";
 function Names({name1,name2, setName1, setName2}) {
   return (
     <>
-      <label>Your Name</label>
+      <label htmlFor="your-name">Your Name</label>
       <input type="text"
      value={name1}
      onChange={(e) => setName1(e.target.value)}
       />
-      <label>Crush's Name</label>
+      <label htmlFor="crush-name">Crush's Name</label>
       <input type="text"
       value={name2} 
       onChange={(e) => setName2(e.target.value)}
